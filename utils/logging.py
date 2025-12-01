6a68fe0b6e093c0fd7c7750d037d69cf7038877b
Shubham Meshram <shubmeshaws@gmail.com>
2025-12-01 12:05:32 +0000

    feat: add logging utility module

---
+++ utils/logging.py
@@ -0,0 +1,35 @@
+```python
+import logging
+import os
+
+# Set up logging configuration
+logging.basicConfig(
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO
+)
+
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def warn(self, message):
+        self.logger.warning(message)
+
+def get_logger(name):
+    return Logger(name)
+
+# Example usage:
+# logger = get_logger(__name__)
+# logger.info('This is an info message')
+# logger.error('This is an error message')
+```

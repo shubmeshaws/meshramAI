d2128b817913a6733ae33ad5a69227da8c192707
Shubham Meshram <shubmeshaws@gmail.com>
2025-12-02 06:59:36 +0000

    feature: add validation utility file

--- utils/validation_utils.py
+++ utils/validation_utils.py
@@ -1,36 +1,34 @@
 ```python
+"""
+Validation utility functions.
+"""
+
 import re
 import ipaddress
-import boto3
-from botocore.exceptions import ClientError
 
-def validate_aws_credentials(access_key, secret_key):
+def validate_aws_region(region):
     """
-    Validate AWS credentials by attempting to list S3 buckets.
+    Validate an AWS region.
 
     Args:
-        access_key (str): AWS access key.
-        secret_key (str): AWS secret key.
+        region (str): The region to validate.
 
     Returns:
-        bool: True if credentials are valid, False otherwise.
+        bool: True if the region is valid, False otherwise.
     """
-    try:
-        s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
-        s3.list_buckets()
-        return True
-    except ClientError:
-        return False
+    # List of valid AWS regions
+    valid_regions = ["us-east-1", "us-west-2", "eu-west-1", "ap-southeast-1"]
+    return region in valid_regions
 
 def validate_ip_address(ip):
     """
     Validate an IP address.
 
     Args:
-        ip (str): IP address to validate.
+        ip (str): The IP address to validate.
 
     Returns:
-        bool: True if IP address is valid, False otherwise.
+        bool: True if the IP address is valid, False otherwise.
     """
     try:
         ipaddress.ip_address(ip)
@@ -43,10 +41,10 @@ def validate_cidr_block(cidr):
     Validate a CIDR block.
 
     Args:
-        cidr (str): CIDR block to validate.
+        cidr (str): The CIDR block to validate.
 
     Returns:
-        bool: True if CIDR block is valid, False otherwise.
+        bool: True if the CIDR block is valid, False otherwise.
     """
     try:
         ipaddress.ip_network(cidr, strict=False)
@@ -54,42 +52,36 @@ def validate_cidr_block(cidr):
     except ValueError:
         return False
 
-def validate_region(region):
+def validate_s3_bucket_name(bucket_name):
     """
-    Validate an AWS region.
+    Validate an S3 bucket name.
 
     Args:
-        region (str): AWS region to validate.
+        bucket_name (str): The bucket name to validate.
 
     Returns:
-        bool: True if region is valid, False otherwise.
+        bool: True if the bucket name is valid, False otherwise.
     """
-    valid_regions = ['us-east-1', 'us-west-1', 'us-west-2', 'eu-west-1', 'eu-central-1', 'ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2']
-    return region in valid_regions
+    # S3 bucket name must be between 3 and 63 characters long
+    # and must be a valid DNS name
+    if len(bucket_name) < 3 or len(bucket_name) > 63:
+        return False
+    if not re.match("^[a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?$", bucket_name):
+        return False
+    return True
 
-def validate_access_key(access_key):
+def validate_ec2_instance_id(instance_id):
     """
-    Validate an AWS access key.
+    Validate an EC2 instance ID.
 
     Args:
-        access_key (str): AWS access key to validate.
+        instance_id (str): The instance ID to validate.
 
     Returns:
-        bool: True if access key is valid, False otherwise.
+        bool: True if the instance ID is valid, False otherwise.
     """
-    pattern = r'^AKIA[0-9A-Z]{16}$'
-    return bool(re.match(pattern, access_key))
-
-def validate_secret_key(secret_key):
-    """
-    Validate an AWS secret key.
-
-    Args:
-        secret_key (str): AWS secret key to validate.
-
-    Returns:
-        bool: True if secret key is valid, False otherwise.
-    """
-    pattern = r'^[a-zA-Z0-9/+=]{40}$'
-    return bool(re.match(pattern, secret_key))
+    # EC2 instance ID must be in the format i-xxxxxxxxxxxxxxxxx
+    if not re.match("^i-[a-f0-9]{8,17}$", instance_id):
+        return False
+    return True
 ```

92a3488af257a860d6fac8b954cb49e9219c890f
Shubham Meshram <shubmeshaws@gmail.com>
2026-01-05 18:22:07 +0000

    refactor: improve validation function to return boolean value

--- modules/vpc.sh
+++ modules/vpc.sh
@@ -4,9 +4,16 @@ VPC_ID_PATTERN="^vpc-[a-z0-9]{17}$"
 function validate_vpc_id() {
   local vpc_id="$1"
   if ! [[ "$vpc_id" =~ $VPC_ID_PATTERN ]]; then
+    return 1
+  fi
+  return 0
+}
+
+function validate_and_handle_vpc_id() {
+  local vpc_id="$1"
+  if ! validate_vpc_id "$vpc_id"; then
     log_error "Invalid VPC ID: $vpc_id. A valid VPC ID should be in the format 'vpc-xxxxxxxxxxxxxxxxx' where 'x' is a lowercase letter or number. Please check the ID and try again."
     show_vpc_help
-    return 1
   fi
 }
 ```
